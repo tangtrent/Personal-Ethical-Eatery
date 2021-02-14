139a92839e909d4320933a0cea08a5a36179186d
Matthew Starcher <mlstarcher@gmail.com>
2021-02-14 09:32:56 -0800

    Establishing link to github

---
+++ src/Components/AboutUs.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class AboutUs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="main">
+          This is the about us component
+      </div>
+    )
+  }
+}
