50f264e6a1945fda817a89e3d3473c79ca6ccd36
tangtrent <tangtrent@gmail.com>
2021-02-19 14:08:22 -0800

    created go back pages on modal page

--- src/Components/CreateRestaurant/ModalPage.js
+++ src/Components/CreateRestaurant/ModalPage.js
@@ -19,19 +19,42 @@ export default function ModalPage({ editRestaurantId }) {
   const [zip, setZip] = useState('');
   const [phone, setPhone] = useState('');
   const [email, setEmail] = useState('');
+  const [about, setAbout] = useState('');
   const history = useHistory();
-  let aboutRef = useRef();
 
-  const handleShow = () => setShowFirst(true);
+  const handleShow = () => { setShowFirst(true); setShowNext(false)}
   const handleNext = () => {
     setShowFirst(false);
     setShowNext(true);
   }
 
   const handleChange = (e) => {
-    const value = e.target.value;
-
-    eval(`set${e.target.title}('${value}')`);
+    switch(e.target.title) {
+      case 'Name':
+        setName(e.target.value);
+        break;
+      case 'StreetNumber':
+        setStreetNumber(e.target.value);
+        break;
+      case 'City':
+        setCity(e.target.value);
+        break;
+      case 'State':
+        setState(e.target.value);
+        break;
+      case 'Zip':
+        setZip(e.target.value);
+        break;
+      case 'Phone':
+        setPhone(e.target.value);
+        break;
+      case 'Email':
+        setEmail(e.target.value);
+        break;
+      case 'About':
+        setAbout(e.target.value);
+        break;
+    }
   }
 
   const handleAdd = () => {
@@ -65,7 +88,7 @@ export default function ModalPage({ editRestaurantId }) {
   const handleSubmit = () => {
     firestore.collection('restaurants').add({
       name: name,
-      aboutUs: aboutRef.current.value,
+      aboutUs: about,
       address: {streetNumber: streetNumber,
                 state: state,
                 city: city,
@@ -96,40 +119,41 @@ export default function ModalPage({ editRestaurantId }) {
       console.error(err)
     }
   }, [])
-
+  console.log(name)
   return (
     <>
       <Modal show={showFirst} onHide={() => {}}>
         <Modal.Header>
           <Modal.Title>Tell us about your restaurant</Modal.Title>
+          <Button href='/dashboard' variant='danger' size='sm'>Go back</Button>
         </Modal.Header>
         <Form>
           <Form.Group className='mt-3'>
             <Col sm='11'>
               <Form.Label className='mt-2'>Restaurant Name</Form.Label>
-              <Form.Control title='Name' placeholder='Name' onChange={handleChange}></Form.Control>
+              <Form.Control title='Name' placeholder='Name' onChange={handleChange} value={name}></Form.Control>
               <Form.Label className='mt-2'>Address</Form.Label>
-              <Form.Control className='mt-2' title='StreetNumber' placeholder='1234 W. Main St.' onChange={handleChange}></Form.Control>
+              <Form.Control className='mt-2' title='StreetNumber' placeholder='1234 W. Main St.' onChange={handleChange} value={streetNumber}></Form.Control>
               <Form.Row className='mt-2'>
                 <Form.Group as={Col} controlId="formGridCity">
                   <Form.Label>City</Form.Label>
-                  <Form.Control className='mt-2' title='City' onChange={handleChange}></Form.Control>
+                  <Form.Control className='mt-2' title='City' onChange={handleChange} value={city}></Form.Control>
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridState">
                   <Form.Label>State</Form.Label>
-                  <Form.Control className='mt-2' title='State' onChange={handleChange}></Form.Control>
+                  <Form.Control className='mt-2' title='State' onChange={handleChange} value={state}></Form.Control>
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridZip">
                   <Form.Label>Zip</Form.Label>
-                  <Form.Control className='mt-2' title='Zip' onChange={handleChange}></Form.Control>
+                  <Form.Control className='mt-2' title='Zip' onChange={handleChange} value={zip}></Form.Control>
                 </Form.Group>
               </Form.Row>
               <Form.Label>Phone</Form.Label>
-              <Form.Control title='Phone' placeholder='(###) ### ####' onChange={handleChange}></Form.Control>
+              <Form.Control title='Phone' placeholder='(###) ### ####' onChange={handleChange} value={phone}></Form.Control>
               <Form.Label className='mt-2'>Email</Form.Label>
-              <Form.Control type='email' title='Email' placeholder='Enter email' onChange={handleChange}></Form.Control>
+              <Form.Control type='email' title='Email' placeholder='Enter email' onChange={handleChange} value={email}></Form.Control>
             </Col>
             <div className='mt-5'>
               <Button className='m-auto' variant='danger' style={{display: 'block', height: '7vh', width: '20vh'}} onClick={handleNext}>Next</Button>
@@ -141,13 +165,14 @@ export default function ModalPage({ editRestaurantId }) {
 
       <Modal show={showNext} onHide={() => {}}>
         <Modal.Header>
-          <Modal.Title>Let's get deeper</Modal.Title>
+          <Modal.Title>Tell us more</Modal.Title>
+          <Button variant='danger' size='sm' onClick={handleShow}>Go back</Button>
         </Modal.Header>
         <Form>
           <Form.Group className='mt-3'>
             <Col sm='11'>
               <Form.Label className='mt-2'>Your story</Form.Label>
-              <Form.Control as='textarea' placeholder='We are a family based...' ref={aboutRef}></Form.Control>
+              <Form.Control as='textarea' title='About' placeholder='We are a family based...' onChange={handleChange} value={about}></Form.Control>
               <Form.Row className='ml-3'>
                 <Form.Group as={Row} controlId="formGridCity">
                   <Form.Label className='mt-2'>What kind of food?</Form.Label>
