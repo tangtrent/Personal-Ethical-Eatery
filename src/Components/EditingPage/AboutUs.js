842dca86acbbf60d1db61e2591e11079f8c41056
Jason Le <jasonle87@gmail.com>
2021-02-20 15:59:48 -0800

    more edits to contact and about us

--- src/Components/EditingPage/AboutUs.js
+++ src/Components/EditingPage/AboutUs.js
@@ -3,6 +3,7 @@ import { Container, Row, Navbar, Nav, Form, FormControl, Button, Card, Figure, I
 export default function AboutUs(props) {
   return(
     <Card className="mx-auto" style={{ width: "500px", border: "0px" }}>
+      <img src={props.item.restaurantImgUrl}></img>
       {props.item.aboutUs}
       <Button variant="primary" size="sm" className="mt-5" onClick={props.handleOpen}>Edit</Button>
     </Card>
--- src/Components/EditingPage/AboutUsModal.js
+++ src/Components/EditingPage/AboutUsModal.js
@@ -1,9 +1,11 @@
-import { Container, Row, Navbar, Nav, Form, FormControl, Button, Card, Figure, Modal, Badge } from 'react-bootstrap'
+import { Container, Row, Navbar, Nav, Form, FormControl, Button, Card, Figure, Modal, Badge, ProgressBar } from 'react-bootstrap'
 import { useState } from 'react'
-import { firestore } from '../../firebase'
+import { firestore, storage } from '../../firebase'
 
 export default function AboutUsModal(props) {
   const [aboutUs, setAboutUs] = useState('')
+  const [restaurantImgUrl, setRestaurantImgUrl] = useState('')
+  const [progress, setProgress] = useState(0)
   const [warning, setWarning] = useState(false)
 
   let query = firestore.collection("restaurants").doc(props.restaurantId);
@@ -15,20 +17,44 @@ export default function AboutUsModal(props) {
   }
 
   const handleSubmit = () => {
-    if (aboutUs.length === 0) {
+    if (aboutUs.length === 0 || restaurantImgUrl.length === 0) {
       setWarning(true)
     } else {
       setWarning(false)
       query.update({
-        aboutUs: aboutUs
+        aboutUs: aboutUs,
+        restaurantImgUrl: restaurantImgUrl
       })
       .then(() => {
         props.handleClose()
+        setProgress(0)
+        setAboutUs('')
+        setRestaurantImgUrl('')
       })
       .catch(() => {
         console.error('Update Failure')
       })
     }
+  }
+
+  const handleFile = (e) => {
+    const file = e.target.files[0];
+    const metadata = {
+      contentType: file.type
+    };
+    storage.ref().child(file.name).put(file, metadata)
+      .on('state_changed' , (snapshot => {
+        setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+      }), (err) => {
+        console.error(err)
+      }, () => {
+        storage.ref().child(file.name).getDownloadURL()
+        .then(url => {
+          console.log(url)
+          setRestaurantImgUrl(url);
+        })
+      })
+
   }
 
   return(
@@ -41,6 +67,11 @@ export default function AboutUsModal(props) {
       </Modal.Header>
 
       <Modal.Body>
+      <Form.Group className='mt-3' onSubmit={() => handleFile()}>
+            <Form.Label>Upload Image</Form.Label>
+            <Form.File.Input onChange={handleFile}></Form.File.Input>
+            <ProgressBar now={progress} className='mt-2'/>
+          </Form.Group>
         {warning && <Badge variant='secondary'>Missing Fields</Badge>}
       </Modal.Body>
 
@@ -48,6 +79,9 @@ export default function AboutUsModal(props) {
         <Button variant="secondary" onClick={() => {
           setWarning(false)
           props.handleClose()
+          setProgress(0)
+          setAboutUs('')
+          setRestaurantImgUrl('')
         }}>Close</Button>
         <Button variant="primary" onClick={handleSubmit}>Save changes</Button>
       </Modal.Footer>
