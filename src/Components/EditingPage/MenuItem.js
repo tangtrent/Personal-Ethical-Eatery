96bb06a22efd70637a526b927aac250cb599b211
tangtrent <tangtrent@gmail.com>
2021-02-20 12:18:52 -0800

    Added file upload through firebase storage. Has a progress bar telling when the file has finished uploading

--- src/Components/EditingPage/MenuItem.js
+++ src/Components/EditingPage/MenuItem.js
@@ -7,7 +7,7 @@ export default function MenuItem(props) {
         return (
           <Card className="mb-2 p-2 d-flex" style={{}}>
             <div className="d-flex flex-row">
-              <img src={item.itemImgUrl}  style={{ width: '25vh', objectFit: 'contain' }}/>
+              <img src={item.itemImgUrl}  style={{ width: '300px', objectFit: 'contain' }}/>
               <Card.Body className='d-flex flex-column justify-content-between align-top'>
                 <div className='d-flex justify-content-between p-4 align-top' style={{ backgroundColor: '#e9ecef' }}>
                   <span>{item.name}</span>
