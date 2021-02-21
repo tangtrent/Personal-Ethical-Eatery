ce78e597dbfcbd967b8ef1d903ef96cd6ca498c3
Jason Le <jasonle87@gmail.com>
2021-02-20 16:10:24 -0800

    final commit hopefully

--- src/Components/EditingPage/ContactModal.js
+++ src/Components/EditingPage/ContactModal.js
@@ -3,6 +3,8 @@ import { useState } from 'react'
 import { firestore } from '../../firebase'
 
 export default function ContactModal(props) {
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
   const [street, setStreet] = useState('')
   const [city, setCity] = useState('')
   const [stait, setStait] = useState('')
@@ -25,18 +27,32 @@ export default function ContactModal(props) {
   const handleZip = (e) => {
     setZip(e.target.value)
   }
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+  const handlePhone =(e) => {
+    setPhone(e.target.value)
+  }
 
   const handleSubmit = () => {
-    if (street.length === 0 || city.length === 0 || stait.length === 0 || zip.length === 0) {
+    if (street.length === 0 || city.length === 0 || stait.length === 0 || zip.length === 0 || email.length === 0 || phone.length === 0) {
       setWarning(true)
     } else {
       setWarning(false)
       var newAddress = {streetNumber: street, city: city, state: stait, zip: zip}
       query.update({
-        address: newAddress
+        address: newAddress,
+        email: email,
+        phone: phone
       })
       .then(() => {
         props.handleClose()
+        setStreet('')
+        setCity('')
+        setStait('')
+        setZip('')
+        setEmail('')
+        setPhone('')
       })
       .catch(() => {
         console.error('Update Failure')
@@ -48,12 +64,20 @@ export default function ContactModal(props) {
     <Modal show={props.show} onHide={() => {
       setWarning(false)
       props.handleClose()
+      setStreet('')
+      setCity('')
+      setStait('')
+      setZip('')
+      setEmail('')
+      setPhone('')
     }}>
       <Modal.Header closeButton>
-        <textarea className='w-100 h-100' placeholder='Street' onChange={handleStreet}/>
+        <input placeholder='Email' onChange={handleEmail}/>
+        <input placeholder='Phone' onChange={handlePhone}/>
       </Modal.Header>
 
       <Modal.Body>
+        <textarea className='w-100 h-100' placeholder='Street' onChange={handleStreet}/>
         <input placeholder='City' onChange={handleCity}/>
         <input placeholder='State' onChange={handleStait}/>
         <input placeholder='Zip' onChange={handleZip}/>
@@ -64,6 +88,12 @@ export default function ContactModal(props) {
         <Button variant="secondary" onClick={() => {
           setWarning(false)
           props.handleClose()
+          setStreet('')
+          setCity('')
+          setStait('')
+          setZip('')
+          setEmail('')
+          setPhone('')
         }}>Close</Button>
         <Button variant="primary" onClick={handleSubmit}>Save changes</Button>
       </Modal.Footer>
